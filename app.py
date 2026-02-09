aaf2bc8aeacc0b69139f9d10411a39e1196608d7
DELL <iamshayanjaved@gmail.com>
2026-02-09 06:34:59 -0800

    chages2

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import pickle
-import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
+import string
 import nltk
 
-nltk.download('stopwords')
-nltk.download('punkt')
+@st.cache_resource
+def download_nltk():
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    nltk.download('stopwords')
+
+download_nltk()
+
 
 
 ps = PorterStemmer()
-
+stop_words = set(stopwords.words('english'))
 
 def transform_text(text):
     text = text.lower()
@@ -20,20 +25,8 @@ def transform_text(text):
     y = []
     for i in text:
         if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
+            if i not in stop_words and i not in string.punctuation:
+                y.append(ps.stem(i))
 
     return " ".join(y)
 
